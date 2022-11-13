fd4f0582c7a7d9e57b0a2cf85566e9654d4b75a8
Огоньков Алексей Михайлович <AOgonkov@vimpelcom.ru>
2022-11-13 11:58:23 +0300

    ДЗ четвертый семинар

---
+++ Четвертый семинар/hw_4.1/Program.cs
@@ -0,0 +1,44 @@
+﻿//Задача 1: Напишите цикл, который принимает на вход два числа (A и B) и 
+//возводит число A в натуральную степень B.
+//3, 5 -> 243 (3⁵)
+//2, 4 -> 16
+int Prompt(string message)                 //определяем метод ввода
+{
+    Console.Write(message);          
+    return int.Parse(Console.ReadLine());   // считать строку с консоли и преобразовать в целое значение
+}
+bool ValidateNumber(int number1, int number2)                  //определяем проверку
+{
+    if (number1 == 0 & number2 == 0)
+    {
+        Console.WriteLine("Не имеет смысла");
+        return false;
+    }
+    if (number2 == 0)
+    {
+        Console.WriteLine($"{number1}, {number2} -> 1"); 
+        return false; 
+    }
+    if (number1 < 0 || number2 < 0)
+    {
+        Console.WriteLine("Числа должны быть положительными");
+        return false;
+    }
+    return true;
+}
+int Stepen(int num1, int num2)             //определяем функцию возведения в степень
+{
+    int result = num1;
+    for (int i = 1; i<num2; i++)                 // цикл for
+    {
+        result = result*num1;
+    }
+    return result;
+}
+int A=Prompt("Введите число: ");
+int B=Prompt("Введите степень: ");
+if (ValidateNumber(A,B)) 
+{
+    int result = Stepen(A,B);
+    Console.WriteLine($"{A}, {B} -> {result}");
+}
---
+++ Четвертый семинар/hw_4.2/Program.cs
@@ -0,0 +1,30 @@
+﻿// Задача 2: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+int Prompt(string message)                 //определяем метод ввода
+{
+    Console.Write(message);          
+    return int.Parse(Console.ReadLine());   // считать строку с консоли и преобразовать в целое значение
+}
+bool ValidateNumber(int number)                  //определяем проверку
+{
+    if (number <= 0)
+    {
+        Console.WriteLine("Это не положительное число");
+        return false;
+    }
+    return true;
+}
+int number=Prompt("Введите положительное число: ");
+if (ValidateNumber(number)) 
+{
+    int sum = 0;
+    do
+    {
+        sum = sum + number%10;                  //добавляем последнюю цифру к сумме
+        number = number/10;                     //сокращаем число на 1 разряд
+    }
+    while (number >= 1);                         //условие - больше цифр не осталось
+Console.WriteLine($"Сумма цифр = {sum}");
+}
