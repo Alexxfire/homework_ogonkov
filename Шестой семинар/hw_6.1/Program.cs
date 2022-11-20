fa9f66e20502902f1200f30fb9eabc206504ceaf
Огоньков Алексей Михайлович <AOgonkov@vimpelcom.ru>
2022-11-20 14:54:57 +0300

    дз шестой семинар

---
+++ Шестой семинар/hw_6.1/Program.cs
@@ -0,0 +1,36 @@
+﻿// Задача 1: Пользователь вводит с клавиатуры M чисел. 
+//Посчитайте, сколько чисел больше 0 ввёл пользователь.
+// 0, 7, 8, -2, -2 -> 2
+// 1, -7, 567, 89, 223-> 3
+void PrintArray(int[] array)                    //определяем метод печати массива
+{
+    Console.Write($"[{array[0]}, ");
+    for (int i = 1; i < array.Length-1; i++)
+    {
+        Console.Write($"{array[i]}, ");
+    }
+    Console.WriteLine($"{array[array.Length-1]}] ");
+}
+int[] NewArray()                                //определяем метод заполнения массива 
+{
+    int[] tempArray = new int[5];
+    for (int i = 0; i < tempArray.Length; i++)
+    {
+        tempArray[i] = int.Parse(Console.ReadLine());
+    }
+    return tempArray;
+}
+int Poisk (int[] array)                  //определяем метод поиска положительных чисел
+{
+    int pol = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] > 0)
+        pol = pol+1;
+    }
+    return pol;
+}
+Console.WriteLine("Введите 5 чисел. Ввод каждого числа подтвердите клавишей Enter");
+int[] array1 = NewArray();
+PrintArray(array1);
+Console.WriteLine($"-> {Poisk(array1)}");
---
+++ Шестой семинар/hw_6.2/Program.cs
@@ -0,0 +1,23 @@
+﻿// Задача 2: Напишите программу, которая найдёт точку пересечения двух прямых, 
+// заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; 
+// значения b1, k1, b2 и k2 задаются пользователем.
+// b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+float Prompt(string message)                 //определяем метод ввода
+{
+    Console.Write(message);          
+    return float.Parse(Console.ReadLine());   // считать строку с консоли и преобразовать в целое значение
+}
+float[] PoiskPer(float b1, float k1, float b2, float k2)
+{
+    float[] array = new float[2];
+    array[0] = (b2-b1)/(k1-k2);
+    array[1] = array[0]*k1+b1;
+    return array;
+}
+Console.Write("Введите параметры прямых линий определяемых уравнениями: \n y = k1 * x + b1, y = k2 * x + b2 \n"); 
+float b1=Prompt("Введите b1: ");
+float k1=Prompt("Введите k1: ");
+float b2=Prompt("Введите b2: ");
+float k2=Prompt("Введите k2: ");
+Console.WriteLine($"Координата х пересечения линий = {PoiskPer(b1,k1,b2,k2)[0]}");
+Console.WriteLine($"Координата y пересечения линий = {PoiskPer(b1,k1,b2,k2)[1]}");
